409cebd1e49544b42a632e4aa3368184503823e4
jharaujoads2502 <jharaujoads@gmail.com>
2021-01-06 09:23:34 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/jha/cursomc/domain/Cliente.java
+++ src/main/java/com/jha/cursomc/domain/Cliente.java
@@ -35,6 +35,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 		
 	}
@@ -127,6 +130,14 @@ public class Cliente implements Serializable{
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
 		this.tipo = tipo.getCod();
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 }
---
+++ src/main/java/com/jha/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,12 @@
+package com.jha.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jha.cursomc.domain.Pagamento;
+
+@Repository
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
