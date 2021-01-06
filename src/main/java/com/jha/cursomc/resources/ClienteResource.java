da375499967b12eb9d4ff17a8200cce9d21236c2
jharaujoads2502 <jharaujoads@gmail.com>
2021-01-06 20:50:12 -0300

    inserindo um novo cliente com o POST

--- src/main/java/com/jha/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jha/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jha.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jha.cursomc.domain.Cliente;
 import com.jha.cursomc.dto.ClienteDto;
+import com.jha.cursomc.dto.ClienteNewDTO;
 import com.jha.cursomc.services.ClienteService;
 
 @RestController
@@ -71,6 +74,16 @@ public class ClienteResource {
 		Page<ClienteDto> listDto = list.map(obj -> new ClienteDto(obj));
 		return ResponseEntity.ok().body(listDto);
 		
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/jha/cursomc/services/ClienteService.java
+++ src/main/java/com/jha/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.jha.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.jha.cursomc.domain.Cidade;
 import com.jha.cursomc.domain.Cliente;
+import com.jha.cursomc.domain.Endereco;
+import com.jha.cursomc.domain.enums.TipoCliente;
 import com.jha.cursomc.dto.ClienteDto;
+import com.jha.cursomc.dto.ClienteNewDTO;
+import com.jha.cursomc.repositories.CidadeRepository;
 import com.jha.cursomc.repositories.ClienteRepository;
+import com.jha.cursomc.repositories.EnderecoRepository;
 import com.jha.cursomc.services.exceptions.DataIntegrityException;
 import com.jha.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,7 +28,21 @@ import com.jha.cursomc.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 	
 	@Autowired
+    private CidadeRepository cidadeRepository;
+	
+	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
 
 	public Cliente find(Integer id) {
 		
@@ -62,10 +84,27 @@ public class ClienteService {
 		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
 	}
 	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+	
 	private void updateData(Cliente newObj,Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	
 	
 	
 }
