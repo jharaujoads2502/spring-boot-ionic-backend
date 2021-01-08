58d60818f089ad961ba617e6c68b3888e0778894
jharaujoads2502 <jharaujoads@gmail.com>
2021-01-07 21:15:08 -0300

    Email HTML

--- src/main/java/com/jha/cursomc/services/EmailService.java
+++ src/main/java/com/jha/cursomc/services/EmailService.java
@@ -1,12 +1,16 @@
 package com.jha.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.jha.cursomc.domain.Pedido;
-
 public interface EmailService {
-
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
